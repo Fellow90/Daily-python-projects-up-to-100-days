af864e6c4b52cdc46fb169a4d189b7f18565c912
Fellow90 <119867781+Fellow90@users.noreply.github.com>
2022-12-07 19:24:51 +0545

    Add files via upload

---
+++ 1--Band Name Generator.py
@@ -0,0 +1,5 @@
+#Demo project for like generating the band name
+print("Okay! You ready for your band name?")
+birthplace = input("Timi janmeko thau ko naam lekha ta babu:")
+bullyFactor = input("Timi lai sano maa jiskaune naam chai k ho?")
+print("Timilai chinaune naam yesto raheko chha:",birthplace," ko ",bullyFactor)
